ded71c83830401ec5676958fa1f6e1ff3c3c6168
Dhrupad <90180213+Dhrupad-Vijay@users.noreply.github.com>
2021-10-28 08:52:39 +0530

    Update one_vs_all.m

--- Week-4 Assignments/codes/one_vs_all.m
+++ Week-4 Assignments/codes/one_vs_all.m
@@ -1,67 +1,21 @@
+% [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels logistic regression classifiers and returns all
+% the classifiers in a matrix all_theta, where the i-th row of all_theta corresponds to the classifier for label i
+
 function [all_theta] = oneVsAll(X, y, num_labels, lambda)
-%ONEVSALL trains multiple logistic regression classifiers and returns all
-%the classifiers in a matrix all_theta, where the i-th row of all_theta 
-%corresponds to the classifier for label i
-%   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
-%   logistic regression classifiers and returns each of these classifiers
-%   in a matrix all_theta, where the i-th row of all_theta corresponds 
-%   to the classifier for label i
 
-% Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+[m, n] = size(X);                                   % Assigning the length and width of X, respectively
+X = [ones(m, 1) X];                                 % Adding the usual column of ones to the dataset
+initial_theta = zeros(n + 1, 1);                    % Initializing a matrix for the theta values - currently filled with zeros
+options = optimset('GradObj', 'on', 'MaxIter', 50); % Setting the options for the advanced optimization function written by Professor Andrew and team
+all_theta = []                                      % Empty matrix, which we will fill with the thetas returned by the advanced optimization function
 
-% You need to return the following variables correctly 
-all_theta = zeros(num_labels, n + 1);
+for i = 1:num_labels                                % Loops for the amout of classifiers we wish to train
 
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
-
-for c = 1:num_labels
-
-    temp_y = y == c;
-    temp_zz = all_theta(c,:); temp_zz = temp_zz(:);
-
-    options = optimset('GradObj', 'on', 'MaxIter', 50);
-    [theta] = fmincg (@(t)(lrCostFunction(t, X, temp_y, lambda)), temp_zz, options);
-    all_theta(c,:) = theta';
+[theta] = fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), initial_theta, options); % Advanced optimization function written by Professor Andrew and team. Returns cost and theta parameters.
+all_theta = [all_theta, theta];                     % Concatenating all_theta with the thetas returnes from the advanced optimization function
 
 end
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the following code to train num_labels
-%               logistic regression classifiers with regularization
-%               parameter lambda. 
-%
-% Hint: theta(:) will return a column vector.
-%
-% Hint: You can use y == c to obtain a vector of 1's and 0's that tell you
-%       whether the ground truth is true/false for this class.
-%
-% Note: For this assignment, we recommend using fmincg to optimize the cost
-%       function. It is okay to use a for-loop (for c = 1:num_labels) to
-%       loop over the different classes.
-%
-%       fmincg works similarly to fminunc, but is more efficient when we
-%       are dealing with large number of parameters.
-%
-% Example Code for fmincg:
-%
-%     % Set Initial theta
-%     initial_theta = zeros(n + 1, 1);
-%     
-%     % Set options for fminunc
-%     options = optimset('GradObj', 'on', 'MaxIter', 50);
-% 
-%     % Run fmincg to obtain the optimal theta
-%     % This function will return theta and the cost 
-%     [theta] = ...
-%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-%                 initial_theta, options);
-%
 
-
-
-
-
+all_theta = all_theta'                              % Transposing the theta values so that an num_labels x n + 1 matrix is returned
 
 end
